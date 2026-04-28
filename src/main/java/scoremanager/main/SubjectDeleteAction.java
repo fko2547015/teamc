aa9e9a6d04c4a6529952799634209be573dcff38
abuuun129 <fko2547011@stu.o-hara.ac.jp>
2026-04-28 14:07:39 +0900

    Merge branch 'master' of https://github.com/fko2547015/teamc.git

--- src/main/java/scoremanager/main/SubjectDeleteAction.java
+++ src/main/java/scoremanager/main/SubjectDeleteAction.java
@@ -5,9 +5,16 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import tool.Action;
 
+
 public class SubjectDeleteAction extends Action {
 	@Override
-	public void execute(HttpServletRequest reqest, HttpServletResponse response) throws Exception{
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception{
+		String subjectCd = request.getParameter("subject_cd");
+		String subjectName = request.getParameter("subject_name");
 		
+		request.setAttribute("subject_cd",subjectCd);
+		request.setAttribute("subject_name",subjectName);
+		
+		request.getRequestDispatcher("subject_delete.jsp").forward(request,response);
 	}
 }
