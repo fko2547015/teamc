6a118783189567a794e23c6bcc1805c3a76a0fff
fko2547015 <fko2547015@stu.o-hara.ac.jp>
2026-04-28 14:57:34 +0900

    n

--- src/main/java/dao/Dao.java
+++ src/main/java/dao/Dao.java
@@ -2,12 +2,15 @@ package dao;
 
 import java.sql.Connection;
 
+import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
 public class Dao {
 	static DataSource ds;
 	
 	public Connection getConnection() throws Exception{
+		InitialContext ic=new InitialContext();
+		DataSource ds = (DataSource) ic.lookup("java:comp/env/jdbc/student");
 		return ds.getConnection();
 	}
 
